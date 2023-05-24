c456d6fdde987005a23219d8a05540fef617b2b6
Jessica Mulein <jessica@digitaldefiance.org>
2023-05-24 09:41:56 -0700

    fallback route

--- api/server.js
+++ api/server.js
@@ -46,7 +46,7 @@ app.post('/api/user', (req, res) => {
   res.json("user addedd");
 });
 
-app.get('/', (req,res) => {
+app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, '../soundstorm-react/build/index.html'));
 });
 
