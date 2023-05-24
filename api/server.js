0d4137ea0c625bfdd2358b7320d9c2a36312cf7d
Jessica Mulein <jessica@digitaldefiance.org>
2023-05-24 09:37:09 -0700

    add version endpoint

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+const { exec } = require('child_process');
 const express = require('express');
 const path = require('path');
 const app = express(),
@@ -9,6 +10,29 @@ const users = [];
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../soundstorm-react/build')));
+
+app.get('/api/version', (req, res) => {
+  exec('git describe --tags', (err, stdout, stderr) => {
+      if (err) {
+          res.status(500).send({ message: 'Unable to get current tag version', error: err });
+          return;
+      }
+      const tagVersion = stdout.trim();
+      
+      exec('git rev-parse HEAD', (err, stdout, stderr) => {
+          if (err) {
+              res.status(500).send({ message: 'Unable to get current hash', error: err });
+              return;
+          }
+          const currentHash = stdout.trim();
+          
+          res.send({
+              tagVersion,
+              currentHash
+          });
+      });
+  });
+});
 
 app.get('/api/users', (req, res) => {
   console.log('api/users called!')
